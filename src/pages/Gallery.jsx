64f3730c97c596925ff9a934046a05dcfab7ee6c
gezerm85 <gezerm85@gmail.com>
2025-08-14 15:24:58 +0300

    update

--- src/pages/Gallery.jsx
+++ src/pages/Gallery.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { useState, useEffect, useRef } from 'react';
 import Navbar from '../components/Navbar';
 import Footer from '../components/Footer';
